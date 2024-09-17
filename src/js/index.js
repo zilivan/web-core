b82b55b951159f31c7d5d993b8128b42247c668e
zilivan <stroielectromir@mail.ru>
2024-09-17 21:42:00 +0300

    ver 36

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import '../scss/main.scss';
 import '../scss/proect.scss';
 
@@ -8,3 +9,8 @@ import '../scss/style.scss'
 >>>>>>> parent of fdf21d3 (ver 2)
 
 console.log('Works!')
+=======
+import 'src/scss/proect1.scss'
+
+
+>>>>>>> parent of 46e319e (ver2)
